36b003ad617a162cb302f317f477c079394cc37a
Kennedy Muthengi <133342792+yourdudeken@users.noreply.github.com>
2025-01-28 13:54:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
 import {addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
